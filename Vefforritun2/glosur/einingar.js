e495bfe95bee2f94681fe47eb1bd2b510435004a
Pétur <rutep>
2018-01-25 16:18:53 +0000

    asdf

---
+++ Vefforritun2/glosur/einingar.js
@@ -0,0 +1,23 @@
+
+// Til að doca þá er hægt að nota jsdoc einingar
+// >jsdoc <skrá> // kemur skrá sem heitir out sem inniheldur doc
+
+
+/**
+ * 
+ * @param {string} path - Path to file
+ * @return  {Promise} Promise that represents
+ * Leið til þess að 
+ */
+
+ function TODO(testString){
+     return testString;
+ }
+
+ console.log(TODO('pétur'));
+
+function test(cp){
+    cp('test',' cool'); 
+}
+
+test((z,t) => {console.log(z+t)});
