9f0b573fa2ff450c0cfd6d5607ce8bce5db59427
Pétur D <peturda3@gmail.com>
2018-01-31 07:35:42 +0000

    breitingar

--- Vefforritun2/verkefni1/app.js
+++ Vefforritun2/verkefni1/app.js
@@ -1,8 +1,12 @@
+
+
 const express = require('express');
 const frontmatter = require('frontmatter');
 const fs = require('fs');
 
-const app = express();
+const app = express('article');
+
+
 const hostname = '127.0.0.1';
 const port = 3000;
 
@@ -18,56 +22,10 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 /**
- * lesa skrá
- */
-var artOne = fs.readFileSync('articles/batman-ipsum.md', 'utf8');
-
-/**
- * Parse gögninn í array
- */
-var artOne = frontmatter(artOne);
-
-
-/**
- * Routs
- */
-app.get('/', (req, res) => {
-  res.render('index',{
-    title: "Verkefni 1",
-    artOne: artOne["data"]["title"],
-    artOneDate: artOne["data"]["date"],
-  });
-});
-
-
-app.get('/batman', (req, res) => {
-  res.render('batman',{
-    title: "Verkefni 1"
-  });
-});
-
-app.get('/corporate', (req, res) => {
-  res.render('corporate',{
-    title: "Verkefni 1"
-  });
-});
-
-app.get('/deloren', (req, res) => {
-  res.render('deloren',{
-    title: "Verkefni 1"
-  });
-});
-
-app.get('/lorem', (req, res) => {
-  res.render('lorem',{
-    title: "Verkefni 1"
-  });
-});
-
-
-/**
  * Býr til local þjón með ip töluna 3000
  */
 app.listen(port, hostname, () => {
   console.info(`Server running at http://${hostname}:${port}/`);
 });
+
+require('./articles')(app,fs,frontmatter);
--- Vefforritun2/verkefni1/articles.js
+++ Vefforritun2/verkefni1/articles.js
@@ -1,2 +1,66 @@
 /* útfæra greina virkni */
+/**
+ * 
+ * 
+ */
 
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt();
+module.exports = function(app,fs,frontmatter){
+
+    /**
+     * lesa skrá
+     */
+    var artOne = fs.readFileSync('articles/batman-ipsum.md', 'utf8');
+    var artTwo = fs.readFileSync('articles/corporate-ipsum.md', 'utf-8');
+    /**
+     * Parse gögninn í array
+     */
+    var artOne = frontmatter(artOne);
+    var artTwo = frontmatter(artTwo);
+
+    console.log(md.render(artTwo['content']));
+
+    /**
+     * Routs
+     */
+    app.get('/', (req, res) => {
+    res.render('index',{
+            title: "Verkefni 1",
+            artOne: artOne["data"]["title"],
+            artOneDate: artOne["data"]["date"].substr(0,15),
+            slugBat: artOne["data"]["slug"]
+        });
+    });
+
+    app.get('/corporate', (req, res) => {
+        res.render('corporate',{
+            title: "Verkefni 1"
+        });
+    });
+
+    app.get('/deloren', (req, res) => {
+        res.render('deloren',{
+            title: "Verkefni 1"
+        });
+    });
+
+    app.get('/'+artTwo['data']['slug'], (req, res) => {
+        res.render(artTwo['data']['slug'],{
+            title: artTwo['data']['title'],
+            content: '<div class="articleText">'+md.render(artTwo['content'])
+        });
+    });
+
+
+    app.get('/'+artOne['data']['slug'], (req, res) => {
+        res.render(artOne['data']['slug'],{
+          title: artOne['data']['title'],
+          content: '<div class="articleText">'+md.render(artOne['content'])
+        });
+      });
+}
+
+
+
+  
