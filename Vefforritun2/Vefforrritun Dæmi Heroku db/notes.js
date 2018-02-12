8b10099a4cc91801be500e1271a14a6134d452fa
Pétur <rutep>
2018-02-12 13:20:16 +0000

    message

--- Vefforritun2/Vefforrritun Dæmi Heroku db/notes.js
+++ Vefforritun2/Vefforrritun Dæmi Heroku db/notes.js
@@ -3,34 +3,54 @@ const express = require('express');
 const router  = express.Router();
 const xss = require('xss');
 const connectionString = 
-process.env.DATABASE_URL || 'postgres://@localhost:5000/notes';
+process.env.DATABASE_URL || "postgresql://@localhost/notes";
+// import module
+const sqlite3 = require('sqlite3').verbose();
+// Create db object
+
 
 function ensureLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
       return next();
     }
-  
     return res.redirect('/login');
   }
   
 /**
  * , ensureLoggedIn
  */
-router.get('/admin', (req, res) => {
-    res.render(`notes`,{});
-  });
+router.get('/admin',async (req, res) => {
+    const client = new sqlite3.Database(process.env.DATABASE_URL || './notes.db');
+    await client.all('select * from Notes',function(err,rows){
+    // let notes = fetchNotes();
+    let notes = rows;
+    
+    res.render(`notes`, {notes});
+    });
+});
+
+
+async function fetchNotes(){
+  // const client = new sqlite3.Database(process.env.DATABASE_URL || './notes.db');
+  // const client = new Client({connectionString })
+  // await client.connect();
+  // await client.query('insert into Notes (notes) values($1)',[note]);
+}
 
 async function addNote(note) {
-  const client = new Client({connectionString })
-  await client.connect();
-  // await client.query('INSERT INTO notes (note) VALUES(1,$1)',[note]);
+  const client = new sqlite3.Database(process.env.DATABASE_URL || './notes.db');
+  let stmt = await client.prepare('INSERT INTO Notes VALUES(?,?)');
+  await stmt.run('2',note);
+  
+  // const client = new Client({connectionString })
+  // await client.connect();
+  // const result = await client.query('insert into notes (id,note) values(1,$1)', [note]);
   // await client.end();
 }  
 
 router.post('/addnote',async (req,res) =>{
   const {note} = req.body;
   await addNote(xss(note));
-
   res.redirect('/admin');
 });
 
