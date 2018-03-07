0eaee7fc1176f50df66e1ab72bb257c26b7d155d
Pétur <rutep>
2018-03-07 11:02:13 +0000

    update

--- Compilers/milliÞulusmiður 1/NanoMorphoParser.java
+++ Compilers/milliÞulusmiður 1/NanoMorphoParser.java
@@ -104,14 +104,12 @@ public class NanoMorphoParser
         over(VAR);
         for(;;)
         {
-            varTable.put(getLexeme(),hashCount++);
             over(NAME);
-            vcount++;
             if( getToken1()!=',' ) break;
             over(',');
         }
     }
-    public static int BINOP;
+
     static Object[] expr() throws Exception
     {
         Object[] res = new Object[]{};
@@ -130,29 +128,11 @@ public class NanoMorphoParser
         }
         else
         {
-            return new Object[]{BINOP,binopexpr()};
+            binopexpr();
         }
+        return res;
     }
 
-<<<<<<< HEAD
-    static int CALL = 1111;
-    static Object[]  binopexpr() throws Exception
-    {   
-        Vector<Object[]> res = new Vector<Object[]>();
-        smallexpr();
-        while( getToken1()==OPNAME )
-        {   
-            res.add(new Object[]{CALL,getLexeme(),2});
-            over(OPNAME); smallexpr();
-        }
-        return res.toArray();
-    }
-
-    static int FunCall = 2000;
-    static void smallexpr() throws Exception
-    {
-        Vector<Object> args = new Vector<Object>();
-=======
      /**
      * (MakeVal null)
      * (Push) breitur og viðfaung
@@ -175,11 +155,9 @@ public class NanoMorphoParser
     static Object[] smallexpr() throws Exception
     {
         Object[] res = new Object[]{};
->>>>>>> c4ba1dec0622924979fbbb9c7b9ef3939ba6f165
         switch( getToken1() )
         {
         case NAME:
-            String name = getLexeme();
             over(NAME);
             if( getToken1()=='(' )
             {
@@ -188,7 +166,7 @@ public class NanoMorphoParser
                 {
                     for(;;)
                     {
-                        args.add(expr());
+                        expr();
                         if( getToken1()==')' ) break;
                         over(',');
                     }
@@ -229,7 +207,6 @@ public class NanoMorphoParser
             expr(); over(';');
         }
         over('}');
-
     }
 
     static void generateFunction( Object[] f )
@@ -274,15 +251,6 @@ public class NanoMorphoParser
         if ((int)e[0] == RETURN) {
             emit("(Return)");
             // generateExpr((Object[])e[1]);
-        }
-        if((int)e[0] == BINOP){
-            Object[] args = (Object[])e[1];
-            for(int i = 0; i < args.length; i++){
-                generateExpr((Object[])args[i]);
-            } 
-        }
-        if((int)e[0] == CALL){
-            emit("(Call #\""+e[1]+"[f"+e[2]+"]\" "+e[2]+")");
         }
     }
 
