3b62754b7e33b12a0e653f298283bdf3153f7734
rutep <peturda3@gmail.com>
2018-10-19 14:11:44 +0000

    update

---
+++ Formleg mál og reiknanleiki/skil8/turingMachine.java
@@ -0,0 +1,11 @@
+public class turingMachine {
+
+    public static int left(int x){
+        return x--;
+    }
+
+    public static void main(String args[]){
+        int index = args.length;
+
+    }
+}
