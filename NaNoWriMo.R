8ef12c225d2db8db3b92a874edb903e0b28146cc
rutep <peturda3@gmail.com>
2018-10-27 10:02:14 +0000

    update

---
+++ NaNoWriMo.R
@@ -0,0 +1,18 @@
+timi <- read.table("dagar.txt")
+timi["V2"]
+timi
+
+markmid <- rep(0.75,30)
+dagar <- (1:30)
+x = 1
+
+for(i in markmid){
+    markmid[x] = i * x
+    x = x + 1
+}
+plot(1, type="n", xlab="Dagar", ylab="Tími sem er búinn að skrifa", 
+	ylim=c(0,22.5), xlim=c(0,30))
+lines(markmid, col="blue")
+lines(timi["V1"], col="red")
+lines(timi["V2"])
+title(main="NaNoWriMor")
