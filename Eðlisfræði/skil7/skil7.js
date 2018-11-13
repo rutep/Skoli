9e748d912950cecab824c575ad2325a95556c1ff
rutep <peturda3@gmail.com>
2018-11-13 14:54:38 +0000

    update

---
+++ Eðlisfræði/skil7/skil7.js
@@ -0,0 +1,28 @@
+
+
+// Hringur sem er verið að toga í með krafti F = 48N
+// Krafturinn verkar á toppinn á hringinn til hægri
+// Hver er hröðuninn ?
+
+var m = 8.6, r = 0.27, F = 48;
+var I, W,
+
+// Formúlur 
+/*
+m: massi, r: radius, F: force
+I: moment of inertia if I is large: then system is
+harder to move
+w: angular velocity
+
+I = m*(r^2)
+k = 1/2*I*(w^2)
+
+k = 1/2*m(r^2)(w^2)
+k*2/m*(r^2)=(w^2)
+sqrt(k*2/m*(r^2)) = w
+
+Sem gefur með hornahraðan
+
+*/
+
+console.log("mu");
